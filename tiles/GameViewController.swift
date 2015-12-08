8df32514eb36d68dd6ad3c0a4e8eec3900f4db5a
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-12-08 17:45:16 -0500

    Create a Spite Kit scene and present it in a SKView. Center a background image :)

--- tiles/GameViewController.swift
+++ tiles/GameViewController.swift
@@ -10,24 +10,20 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
+    var scene: GameScene!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
+        let skView = view as! SKView
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+
+        skView.presentScene(scene)
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
 
     override func shouldAutorotate() -> Bool {
@@ -35,19 +31,6 @@ class GameViewController: UIViewController {
     }
 
     override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override func prefersStatusBarHidden() -> Bool {
-        return true
+        return UIInterfaceOrientationMask.AllButUpsideDown
     }
 }
