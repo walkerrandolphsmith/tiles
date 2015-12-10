e1cec2dfab53df78ff7bda26afe166c0f4d28307
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2015-12-09 21:23:10 -0500

    Ensure the game does not begin with sequences of three or more of the same cookie type

--- tiles/Level.swift
+++ tiles/Level.swift
@@ -45,7 +45,17 @@ class Level {
         for row in 0..<numberOfRows {
             for column in 0..<numberOfColumns{
                 if tiles[column, row] != nil {
-                    let cookieType = CookieType.random()
+                    var cookieType : CookieType
+                    repeat {
+                        cookieType = CookieType.random()
+                    }
+                    while (column >= 2 &&
+                        cookies[column - 1, row]?.cookieType == cookieType &&
+                        cookies[column - 2, row]?.cookieType == cookieType)
+                        || (row >= 2 &&
+                        cookies[column, row - 1]?.cookieType == cookieType &&
+                        cookies[column, row - 2]?.cookieType == cookieType)
+
                     let cookie = Cookie(column: column, row: row, cookieType: cookieType)
                     cookies[column, row] = cookie
 
